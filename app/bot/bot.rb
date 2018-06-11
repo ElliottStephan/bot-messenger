5daba1c77714961ac644f69fb17117362e1ef1d1
EC2 Default User <ec2-user@ip-172-31-33-156.ec2.internal>
2018-06-11 20:36:47 +0000

    Wesh

--- app/bot/bot.rb
+++ app/bot/bot.rb
@@ -5,7 +5,7 @@ Facebook::Messenger::Subscriptions.subscribe(access_token: ENV["ACCESS_TOKEN"])
 
 Bot.on :message do |message|
     if message.text.include? "Qui est le plus beau ?"
-        message.reply(text "Elliott est le plus beau")
+        message.reply(text: "Elliott est le plus beau")
     else    
         message.reply(text: 'Hello, human!')
 end
